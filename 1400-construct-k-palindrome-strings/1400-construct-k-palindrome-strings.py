d1657426a27b2b4a068db83cb0194323bbda7754
Sajida Javed <sajidajaved604@gmail.com>
2025-01-11 12:51:52 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if(len(s) == k):
+            return True
+        elif(len(s) < k):
+            return False
+        dic = {}
+        for a in s:
+            if(a not in dic): 
+                dic[a] = 1 
+            else:
+                dic[a]+=1
+        print(dic)
+        singleElement = 0
+        words = 0
+        for key in dic.keys():
+            if dic[key]%2!=0: # odd
+                singleElement += 1
+                if(dic[key] > 1):
+                    words += 2
+            else: #even
+                words += 2
+            if(singleElement > k):
+                return False
+
+        if((words + singleElement)  < k):
+            return False
+        return True
+        
