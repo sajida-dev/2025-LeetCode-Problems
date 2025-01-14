00bc8c89f74d0c6a56cb4bfae0c5060e68878979
Sajida Javed <sajidajaved604@gmail.com>
2025-01-14 23:33:46 +0500

    Time: 7 ms (75%), Space: 17.7 MB (45.05%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -3,10 +3,15 @@ class Solution:
         n = len(A)
         fre = [0]*n
         ans = [0]*n
+        count = 0
         for i in range(n):
             fre[A[i]-1] += 1
             fre[B[i]-1] += 1
-            #print(fre)
-            ans[i] = fre.count(2)
-            #print(ans)
+            if(fre[A[i]-1] == 2):
+                count += 1
+                fre[A[i]-1] = 0
+            if(fre[B[i]-1] == 2):
+                count += 1
+                fre[B[i]-1] = 0
+            ans[i] = count
         return ans
