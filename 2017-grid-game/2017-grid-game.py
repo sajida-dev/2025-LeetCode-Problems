81725982440f15f796e86b1a06b25daf7cb299c1
Sajida Javed <sajidajaved604@gmail.com>
2025-01-21 23:11:24 +0500

    Time: 312 ms (5.25%), Space: 30.5 MB (15.08%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -3,6 +3,8 @@ class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
         pre = copy.deepcopy(grid)
         n=len(grid[0])
+        if(n==1):
+            return 0
         for i in range(2,n):
             pre[0][i] += pre[0][i-1]
             pre[1][n-1-i] += pre[1][n-i]
