19ddb90c735b894d6af7b5c18d70ece0b4c81c94
Sajida Javed <sajidajaved604@gmail.com>
2025-02-12 23:35:51 +0500

    Time: 268 ms (88.98%), Space: 33.9 MB (24.3%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,4 +1,3 @@
-from sortedcontainers import SortedList
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
         dic = {}
@@ -10,13 +9,18 @@ class Solution:
                 val = val//10
             if(sumVal in dic):
                 dic[sumVal][0] += 1
-                dic[sumVal][1].add(x)
+                if(dic[sumVal][2] < dic[sumVal][1]):
+                    if(dic[sumVal][2] < x):
+                        dic[sumVal][2] = x
+                else:
+                    if(dic[sumVal][1] < x):
+                        dic[sumVal][1] = x
             else:
-                dic[sumVal] = [1,SortedList([x])]
+                dic[sumVal] = [1,x,0]
         ans = -1
         for val in dic.values():
             if(val[0]>=2):
-                sumVal = val[1][-1]+val[1][-2]
+                sumVal = val[1]+val[2]
                 if(sumVal > ans):
                     ans = sumVal
         return ans
