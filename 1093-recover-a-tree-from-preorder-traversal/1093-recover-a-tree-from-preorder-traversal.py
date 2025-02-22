3feb8927dee37a20e8b3315b107f4ee277a774f7
Sajida Javed <sajidajaved604@gmail.com>
2025-02-23 01:53:32 +0500

    Time: 19 ms (64.22%), Space: 18.2 MB (96.19%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.py
@@ -0,0 +1,30 @@
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        stack = []
+        index = 0
+
+        while index < len(traversal):
+            depth = 0
+            while index < len(traversal) and traversal[index] == "-":
+                depth += 1
+                index += 1
+
+            value = 0
+            while index < len(traversal) and traversal[index].isdigit():
+                value = value * 10 + int(traversal[index])
+                index += 1
+
+            node = TreeNode(value)
+
+            while len(stack) > depth:
+                stack.pop()
+
+            if stack:
+                if stack[-1].left is None:
+                    stack[-1].left = node
+                else:
+                    stack[-1].right = node
+
+            stack.append(node)
+
+        return stack[0]
