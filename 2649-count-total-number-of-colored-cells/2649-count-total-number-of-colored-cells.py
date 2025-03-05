0a549d10b2419b23ffaa8395a966b3947f171de0
Sajida Javed <sajidajaved604@gmail.com>
2025-03-05 07:08:02 +0500

    Time: 200 ms (10.15%), Space: 17.7 MB (75.19%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -1,8 +1,8 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        totalColored = 1
+        totalColored, diagonalCells = 1,0
         for i in range(2, n+1):
-            if(i-2 > 0):
-                totalColored += ((i-2)*4)
-            totalColored += 4
+            if(i >= 3):
+                diagonalCells += 4
+            totalColored += (4 + diagonalCells)
         return totalColored
