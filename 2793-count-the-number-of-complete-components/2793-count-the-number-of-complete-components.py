bfd3da4919f452594ccfb69ea6b6fdbfe8108f00
Sajida Javed <sajidajaved604@gmail.com>
2025-03-22 23:51:30 +0500

    Time: 72 ms (27.34%), Space: 18.4 MB (33.84%) - LeetHub

---
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = [[]]*n
+        connectedEdges = defaultdict(int)
+
+        for vertex in range(n):
+            graph[vertex] = [vertex]
+
+        for v1, v2 in edges:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+
+        for vertex in range(n):
+            neighbors = tuple(sorted(graph[vertex]))
+            connectedEdges[neighbors] += 1
+        print(graph,connectedEdges)
+        # return sum(1 for ... if ...)
+        return sum(
+            1
+            for neighbors, freq in connectedEdges.items()
+            if len(neighbors) == freq
+        )
