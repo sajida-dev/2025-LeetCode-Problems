a2107b603f75a58d1117eb68559f672f2e04b0d8
Sajida Javed <sajidajaved604@gmail.com>
2026-01-20 21:53:03 +0500

    Time: 0 ms (100%), Space: 20.5 MB (10.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,31 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
         for i in range(0,len(nums)):
-            for j in range(i+1, len(nums)):
-                if(nums[i] + nums[j] == target):
-                    return [i,j]
+            sub = target - nums[i]
+            if sub in dic:
+                return [dic[sub],i]
+            else:
+                dic[nums[i]] = i
+            
+
+
+# dic = {}
+# dic['key'] =value
+# {2:0,}
+
+# 9-7 = 2
+# if  2 is in dic:
+#     return [dic[2],i]
+# else 
+#     dic[2] = index
+
+
+# [3,2,4]
+# 6-3 =3
+# 6-2 = 4
+# 6-4 = 2
+# {3:0,2:1,}
 
 
 # sum = num[0] + num[1]  
@@ -19,4 +41,4 @@ class Solution:
 # range()
 # if single parm: end value
 # if 2 parm : start value, end value
-# if 3 parm : start value, end value, increment 
+# if 3 parm : start value, end value, increment  
